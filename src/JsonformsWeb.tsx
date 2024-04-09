1a6562fae438d1e7064b679a820aed7e8ffc42d6
Carlos Sanchez <a250982us@gmail.com>
2024-04-09 14:31:08 -0500

    Fix Data Issues

--- src/JsonformsWeb.tsx
+++ src/JsonformsWeb.tsx
@@ -1,9 +1,6 @@
-import { Component, ReactNode, createElement, useMemo } from "react";
+import { Component, ReactNode, createElement } from "react";
 import { JsonformsWebContainerProps } from "../typings/JsonformsWebProps";
-import { vanillaRenderers, vanillaCells } from '@jsonforms/vanilla-renderers';
-import { JsonForms } from '@jsonforms/react';
-var get = require('lodash.get');
-
+import { Form } from "../src/component/Form";
 import "./ui/JsonformsWeb.css";
 
 interface AppState {
@@ -26,9 +23,10 @@ const getCircularReplacer = () => {
             }
             seen.add(value);
         }
-        return value; 
+        return value;
     };
 };
+
 
 export class JsonformsWeb extends Component<JsonformsWebContainerProps, AppState> {
     constructor(props: any) {
@@ -44,106 +42,48 @@ export class JsonformsWeb extends Component<JsonformsWebContainerProps, AppState
             errorMessage:"Waiting for the form submition"
         };
     }
-
-
-    
     render(): ReactNode {
-        
-        const onChange = (data:any, errors:any) => {
-            if(errors && errors!=null && errors.length>0 && errors[0]){
-                console.debug(errors);
-                console.error("Using the specified JSON schema, a JSON form error occurred.");
-            }else{
-                console.debug(data);
-                /*
-                
-                if (this.props.mxFormData.value == jsonString) {
-                    this.setState({ errorShow: true });
-           z         this.setState({ errorMessage: "Form values entered match those in the database." });
-                }
-                
-                const jsonString: string = JSON.stringify(JSON.parse(onSubmitJson).formData);
-                */
-                //const onSubmitJson = JSON.stringify(data, getCircularReplacer());
-                //this.state.formData=JSON.stringify(data);
-                //this.props.mxFormData.setValue(onSubmitJson);
+        if(this.state.errorShow){
+            return (
+                <div className="jsonschema-to-jsonform-root">
+                    <div className="JSONViewer">
+                        {this.state.errorShow && (
+                            <div className="alert alert-warning">
+                                <strong>Warning!</strong> {this.state.errorMessage}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            );
+        }else{
+            if(this.state.schema!=="{}" || this.state.uischema!=="{}"){
+                return (                
+                    <Form
+                        schema={this.state.schema}
+                        uischema={this.state.uischema}
+                        initData={this.state.initData}
+                        i18nData={this.state.i18nData}
+                        language={this.state.language}
+                        _onChange={(data:any, errors:any) => {
+                            if(errors && errors!=null && errors.length>0 && errors[0]){
+                                console.debug("Using the specified JSON schema, a JSON form error occurred.");
+                                console.debug(errors);
+                            }else{
+                                const onSubmitJson = JSON.stringify(data, getCircularReplacer());
+                                this.props.mxFormData.setValue(onSubmitJson);    
+                            }
+                        }}
+                    /> 
+                );    
             }
-        };
-
-        let locale=this.state.language;
-        console.debug("Language:"+locale);        
-        if(locale && locale!="none"){
-            console.debug("Step1");
-            const createTranslator = (locale:any) => (key:any, defaultMessage:any) => {            
-                console.debug("StepLost");
-                console.debug(`Key: ${key}, Default Message: ${defaultMessage}`);
-                let myi18nfile=JSON.parse(this.state.i18nData);
-                console.debug(this.state.i18nData);
-                console.debug(myi18nfile);
-                
-                return get(myi18nfile, `${locale}.${key}`, defaultMessage);
-            }
-            console.debug("Step2");
-            let translation=createTranslator(locale);
             
-            /*
-            try{
-                translation = useMemo(() => createTranslator(locale), [locale]);
-            }catch(e){
-                console.debug(e)
-                console.debug(e.message);
-                console.debug("Erroir:"+e.message)
-            }
-            */
-            console.debug("Step3");
-            return (
-                <div className="jsonschema-to-jsonform-root">
-                    <JsonForms
-                        schema={JSON.parse(this.state.schema)}
-                        uischema={JSON.parse(this.state.uischema)}
-                        data={JSON.parse(this.state.initData)}
-                        renderers={vanillaRenderers}
-                        cells={vanillaCells}
-                        i18n={{locale: locale, translate: translation}}
-                        onChange={({ data, errors }) => onChange(data, errors)}
-                    />
-                    <div className="JSONViewer">
-                        {this.state.errorShow && (
-                            <div className="alert alert-warning">
-                                <strong>Warning!</strong> {this.state.errorMessage}
-                            </div>
-                        )}
-                    </div>
-                </div>
-                );
-
-        } else{
-            return (
-                <div className="jsonschema-to-jsonform-root">
-                    <JsonForms
-                        schema={JSON.parse(this.state.schema)}
-                        uischema={JSON.parse(this.state.uischema)}
-                        data={JSON.parse(this.state.initData)}
-                        renderers={vanillaRenderers}
-                        cells={vanillaCells}
-                        onChange={({ data, errors }) => onChange(data, errors)}
-                    />
-                    <div className="JSONViewer">
-                        {this.state.errorShow && (
-                            <div className="alert alert-warning">
-                                <strong>Warning!</strong> {this.state.errorMessage}
-                            </div>
-                        )}
-                    </div>
-                </div>
-                );
-        }       
+        }
     }
 
     componentDidMount(): void {
         const {  mxSchema, mxUiSchema, mxInitData, mxI18n, mxLanguage } = this.props;
 
-        let inputSchema = mxSchema.value ? mxSchema.value.toString() : '{"description": "JSON schema received was empty!","type": "object"}';
+        let inputSchema = mxSchema.value ? mxSchema.value.toString() : '{}';
         let inputUISchema = mxUiSchema.value ? mxUiSchema.value.toString() : "{}";
         let inputInitData = mxInitData.value ? mxInitData.value.toString() : "{}";
         let inputI18n = mxI18n.value ? mxI18n.value.toString() : "{}";
@@ -154,21 +94,22 @@ export class JsonformsWeb extends Component<JsonformsWebContainerProps, AppState
         let eInitData=isValidJSON(inputInitData);
         let e18n=isValidJSON(inputI18n);
         
-        let eMessage;
+        let eMessage="";
         let eShow=false;
         if(eSchema){
-            getErrorMessage("Schema:", eSchema, eMessage)
+            eMessage=getErrorMessage("Schema", eSchema, eMessage);
             eShow=true;
             inputSchema = '{}';
         }
         if(eUiSchema){
-            getErrorMessage("UISchema:", eUiSchema, eMessage)
+            eMessage=getErrorMessage("UISchema", eUiSchema, eMessage);
             eShow=true;
             inputUISchema = '{}';
         }
         if(eInitData){
-            getErrorMessage("InitData:", eInitData, eMessage)
+            eMessage=getErrorMessage("InitData", eInitData, eMessage);
             eShow=true;
+            inputInitData = '{}';
         }
         if(e18n){
             inputI18n = '{}';
@@ -193,34 +134,37 @@ export class JsonformsWeb extends Component<JsonformsWebContainerProps, AppState
             mxInitData.value !== prevProps.mxInitData.value ||
             mxI18n.value!==prevProps.mxI18n.value
         ) {
-            let inputSchema = mxSchema.value?.toString() || '{"description": "JSON schema received was empty!","type": "object"}';
+            let inputSchema = mxSchema.value?.toString() || '{}';
             let inputUISchema = mxUiSchema!.value?.toString() || "{}";
             let inputInitData = mxInitData!.value?.toString() || "{}";
             let inputI18n = mxI18n!.value?.toString() || "{}";
             let inputLanguage = mxLanguage!.value?.toString() || 'none';
 
+            let eMessage="";
+            let eShow=false;
+
             let eSchema=isValidJSON(inputSchema);
             let eUiSchema=isValidJSON(inputUISchema);
             let eInitData=isValidJSON(inputInitData);
             let e18n=isValidJSON(inputI18n);
-            
-            let eMessage;
-            let eShow=false;
+
             if(eSchema){
-                getErrorMessage("Schema:", eSchema, eMessage)
+                eMessage=getErrorMessage("Schema", eSchema, eMessage);
                 eShow=true;
                 inputSchema = '{}';
             }
             if(eUiSchema){
-                getErrorMessage("UISchema:", eUiSchema, eMessage)
+                eMessage=getErrorMessage("UISchema", eUiSchema, eMessage);
                 eShow=true;
                 inputUISchema = '{}';
             }
             if(eInitData){
-                getErrorMessage("InitData:", eInitData, eMessage)
+                eMessage=getErrorMessage("InitData", eInitData, eMessage);
                 eShow=true;
+                inputInitData = '{}';
             }
             if(e18n){
+                console.debug(e18n);
                 inputI18n = '{}';
             }
             this.setState({
@@ -249,11 +193,7 @@ function isValidJSON(jsonString: string) {
     }
 }
 
-function getErrorMessage(_type:string, _error:string|undefined, _message:string|undefined){
-    if (_error) {
-        console.debug(_error);
-        if(!_message) _message=""; 
-        return _message+_type+_error+"\n";
-    }        
-    return _message;
+function getErrorMessage(_type:string, _error:string, _message:string){
+    console.debug(_error);
+    return _message+_type+":"+_error+"\n";
 }
---
+++ src/component/Form.tsx
@@ -0,0 +1,53 @@
+import { createElement, useState } from "react";
+import { vanillaRenderers, vanillaCells } from '@jsonforms/vanilla-renderers';
+import { JsonForms } from '@jsonforms/react';
+var get = require('lodash.get');
+
+export function Form(props:any){
+
+    console.log("Props1:"+props);
+    console.log("PropsS:"+props.schema);
+    console.log("PropsU:"+props.uischema);
+    console.log("PropsD:"+props.initData);
+    const [dato, setDato] = useState(props.initData);   
+
+    if(props.language && props.language!="none"){
+        const createTranslator = (locale:any) => (key:any, defaultMessage:any) => {            
+            let myi18nfile=JSON.parse(props.i18nData);
+            return get(myi18nfile, `${locale}.${key}`, defaultMessage);
+        }
+        let translation=createTranslator(props.language);
+        return ( 
+            <div className="jsonschema-to-jsonform-root">
+                <JsonForms
+                    schema={JSON.parse(props.schema)}
+                    uischema={JSON.parse(props.uischema)}
+                    data={JSON.parse(dato)}
+                    renderers={vanillaRenderers}
+                    cells={vanillaCells}
+                    i18n={{locale: props.language, translate: translation}}
+                    onChange={({ data, errors }) => {
+                        setDato(JSON.stringify(data));
+                        props._onChange(data, errors);
+                    }}
+                />
+            </div>
+        );
+    }else{
+        return ( 
+            <div className="jsonschema-to-jsonform-root">
+                <JsonForms
+                    schema={JSON.parse(props.schema)}
+                    uischema={JSON.parse(props.uischema)}
+                    data={dato}
+                    renderers={vanillaRenderers}
+                    cells={vanillaCells}
+                    onChange={({ data, errors }) => {
+                        setDato(data);
+                        props._onChange(data, errors);
+                    }}
+                />
+            </div>
+        )
+    }
+}
